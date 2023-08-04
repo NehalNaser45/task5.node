8530bc62c1f81d78cd62ab1477ac54179f8874c3
NehalNaser45 <126026006+NehalNaser45@users.noreply.github.com>
2023-08-04 18:10:13 +0300

    Add files via upload

---
+++ public/js/script.js
@@ -0,0 +1,45 @@
+
+let form = document.getElementById('form1')
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    // console.log(document.getElementById('address').value)
+    weatherFunction()
+    form.reset()
+})
+const errorF = document.getElementById('error')
+const locationF = document.getElementById('location')
+const forecastF = document.getElementById('forecast')
+
+// async --> function return promise
+let weatherFunction = async() =>{
+    try{
+        const address = document.getElementById('address').value
+        const res = await fetch('http://localhost:3000/weather?address='+address)
+        const data = await res.json()
+        console.log(data)
+        if(data.error){
+            errorF.innerText = data.error
+            locationF.innerText =""
+            forecastF.innerText =""
+        }
+        else {
+            setTimeout(()=>{
+                locationF.innerText = data.location  
+            },3000)
+            setTimeout(()=>{
+                forecastF.innerText = data.forecast
+
+
+            },7000)
+            setTimeout(()=>{
+                locationF.innerText = data.location  
+            },1000)
+            
+        }
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+
+// 3 
